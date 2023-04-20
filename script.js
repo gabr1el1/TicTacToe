7169a99d88d75eac937f8754a899bf375a8793c6
gabr1el1 <gabrielhgxd@gmail.com>
2023-04-20 10:15:12 -0600

    Game works if a player wins

--- script.js
+++ script.js
@@ -2,24 +2,36 @@ const gameBoard = (function(){
     //variables
     let player1,player2,gameControler,gameStatus = ".".repeat(9).split("");
     //DOM
-    let player1NameInput = document.querySelector(".player-name-1");
-    let player2NameInput = document.querySelector(".player-name-2");
-    let player1SelecInput = document.querySelector(".player-selection-1");
-    let player2SelecInput = document.querySelector(".player-selection-2");
+    let player1NameInput = document.querySelector("#player-name-1");
+    let player2NameInput = document.querySelector("#player-name-2");
+    let player1SelecInput = document.querySelector("#player-selection-1");
+    let player2SelecInput = document.querySelector("#player-selection-2");
+    let formStartGame = document.querySelector("#myForm")
     let startBtn = document.querySelector(".start-button");
+    let restartBtn = document.querySelector(".restart-button");
     let gameSpace = document.querySelector(".gameboard");
     let squares = [];
     //Bind
-    startBtn.addEventListener("click",_init);
+    formStartGame.addEventListener("submit",_init);
+    restartBtn.addEventListener("click",_restart);
+
     //render
     _render();
-    function _init(){
+    function _init(event){
+        event.preventDefault();
         player1 = player(player1NameInput.value,player1SelecInput.value);
         player2 = player(player2NameInput.value,player2SelecInput.value);
         gameControler = gameFlow();
         gameControler.currentPlayer = player1;
-        
+        gameSpace.classList.add("game-ready");
     }
+    function _restart(){
+        gameSpace.classList.remove("game-ready");
+        player1NameInput.value = "";
+        player2NameInput.value = "";
+
+    }
+
     function _render(){
         for(let i=0; i<9; i++){
             let square = document.createElement("div");
@@ -32,15 +44,16 @@ const gameBoard = (function(){
     function _chooseSquare(index){
         
         if(this.innerText==""){
+            this.innerText=gameControler.currentPlayer.selection;
+            gameStatus[index] = gameControler.currentPlayer.selection;
+            _checkGameStatus();
             if(gameControler.currentPlayer==player1){
                 gameControler.currentPlayer=player2;
             }else{
                 gameControler.currentPlayer=player1;
             }
-            this.innerText=gameControler.currentPlayer.selection;
-            gameStatus[index] = gameControler.currentPlayer.selection;
         }
-        _checkGameStatus();
+        
     }
     
     function _checkGameStatus(){
@@ -88,13 +101,13 @@ const gameBoard = (function(){
         );
         let condition8 = 
         (
-            gameStatus[3]==gameControler.currentPlayer.selection &&
-            gameStatus[5]==gameControler.currentPlayer.selection &&
-            gameStatus[7]==gameControler.currentPlayer.selection 
+            gameStatus[2]==gameControler.currentPlayer.selection &&
+            gameStatus[4]==gameControler.currentPlayer.selection &&
+            gameStatus[6]==gameControler.currentPlayer.selection 
         );
         if(condition1 || condition2 || condition3 || condition4 || condition5 ||
             condition6||condition7 || condition8){
-                alert(gameControler.currentPlayer.name);
+                alert(`${gameControler.currentPlayer.name} wins!`);
         }
     }
 })();
